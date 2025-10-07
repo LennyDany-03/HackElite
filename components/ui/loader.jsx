1214f5a116646722ed0153145b288d2a5e0942db
LennyDany-03 <lennydany3@gmail.com>
2025-10-08 03:05:04 +0530

    ver: 0.5 fix: Fixing the Vercel Deployment Error

--- components/ui/loader.jsx
+++ components/ui/loader.jsx
@@ -3,44 +3,33 @@ import { motion } from "motion/react";
 import React from "react";
 
 export const LoaderOne = () => {
-  const transition = (x: number) => {
+  const transition = (x) => {
     return {
       duration: 1,
       repeat: Infinity,
-      repeatType: "loop" as const,
+      repeatType: "loop",
       delay: x * 0.2,
       ease: "easeInOut",
     };
   };
+
   return (
     <div className="flex items-center gap-2">
       <motion.div
-        initial={{
-          y: 0,
-        }}
-        animate={{
-          y: [0, 10, 0],
-        }}
+        initial={{ y: 0 }}
+        animate={{ y: [0, 10, 0] }}
         transition={transition(0)}
         className="h-4 w-4 rounded-full border border-neutral-300 bg-gradient-to-b from-neutral-400 to-neutral-300"
       />
       <motion.div
-        initial={{
-          y: 0,
-        }}
-        animate={{
-          y: [0, 10, 0],
-        }}
+        initial={{ y: 0 }}
+        animate={{ y: [0, 10, 0] }}
         transition={transition(1)}
         className="h-4 w-4 rounded-full border border-neutral-300 bg-gradient-to-b from-neutral-400 to-neutral-300"
       />
       <motion.div
-        initial={{
-          y: 0,
-        }}
-        animate={{
-          y: [0, 10, 0],
-        }}
+        initial={{ y: 0 }}
+        animate={{ y: [0, 10, 0] }}
         transition={transition(2)}
         className="h-4 w-4 rounded-full border border-neutral-300 bg-gradient-to-b from-neutral-400 to-neutral-300"
       />
@@ -49,44 +38,33 @@ export const LoaderOne = () => {
 };
 
 export const LoaderTwo = () => {
-  const transition = (x: number) => {
+  const transition = (x) => {
     return {
       duration: 2,
       repeat: Infinity,
-      repeatType: "loop" as const,
+      repeatType: "loop",
       delay: x * 0.2,
       ease: "easeInOut",
     };
   };
+
   return (
     <div className="flex items-center">
       <motion.div
         transition={transition(0)}
-        initial={{
-          x: 0,
-        }}
-        animate={{
-          x: [0, 20, 0],
-        }}
+        initial={{ x: 0 }}
+        animate={{ x: [0, 20, 0] }}
         className="h-4 w-4 rounded-full bg-neutral-200 shadow-md dark:bg-neutral-500"
       />
       <motion.div
-        initial={{
-          x: 0,
-        }}
-        animate={{
-          x: [0, 20, 0],
-        }}
+        initial={{ x: 0 }}
+        animate={{ x: [0, 20, 0] }}
         transition={transition(0.4)}
         className="h-4 w-4 -translate-x-2 rounded-full bg-neutral-200 shadow-md dark:bg-neutral-500"
       />
       <motion.div
-        initial={{
-          x: 0,
-        }}
-        animate={{
-          x: [0, 20, 0],
-        }}
+        initial={{ x: 0 }}
+        animate={{ x: [0, 20, 0] }}
         transition={transition(0.8)}
         className="h-4 w-4 -translate-x-4 rounded-full bg-neutral-200 shadow-md dark:bg-neutral-500"
       />
@@ -124,7 +102,7 @@ export const LoaderThree = () => {
   );
 };
 
-export const LoaderFour = ({ text = "Loading..." }: { text?: string }) => {
+export const LoaderFour = ({ text = "Loading..." }) => {
   return (
     <div className="relative font-bold text-black [perspective:1000px] dark:text-white">
       <motion.span
@@ -182,7 +160,7 @@ export const LoaderFour = ({ text = "Loading..." }: { text?: string }) => {
   );
 };
 
-export const LoaderFive = ({ text }: { text: string }) => {
+export const LoaderFive = ({ text }) => {
   return (
     <div className="font-sans font-bold [--shadow-color:var(--color-neutral-500)] dark:[--shadow-color:var(--color-neutral-100)]">
       {text.split("").map((char, i) => (
